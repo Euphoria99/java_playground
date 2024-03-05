8e38c76c09491d7c3edc229bc5b193a0e685f972
Pavan Bhat <363pavanbhat@gmail.com>
2024-03-05 22:54:49 +0530

    Statements

---
+++ src/com/basics/Statements.java
@@ -0,0 +1,24 @@
+package com.basics;
+
+public class Statements {
+    public static void main(String[] args) {
+        int a = 10;
+
+        //if statements
+        if(a == 10){
+            System.out.println("Hi there");
+        }
+
+        //while loop
+        int count =1;
+        while(count !=5){
+            System.out.println(count);
+            count++;
+        }
+
+        //for loop
+        for(int newcount =1; newcount !=5; newcount++){
+            System.out.println(count);
+        }
+    }
+}
