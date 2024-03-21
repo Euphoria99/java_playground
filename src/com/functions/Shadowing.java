a90529f57ee81783f300674b41fcedb890020e9c
Pavan Bhat <363pavanbhat@gmail.com>
2024-03-21 16:34:33 +0530

    Shadowing

---
+++ src/com/functions/Shadowing.java
@@ -0,0 +1,15 @@
+package com.functions;
+
+public class Shadowing {
+    static int x =90;
+    public static void main(String[] args) {
+        System.out.println(x); //90 . this will be shadowed at line 8.
+        int x = 40;
+        System.out.println(x);  //40
+        fun();
+    }
+
+    private static void fun() {
+        System.out.println(x); //90
+    }
+}
